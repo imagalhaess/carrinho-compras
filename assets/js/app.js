a029fc38c31e7fd9aab8ad1e92d1a2091de0a169
Isabela M. <magalhaes569@gmail.com>
2025-05-16 15:48:55 -0300

    feat: função de limpar carrinho adicionada

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,33 @@
+let total = 0;
 function adicionar() {
   const selectProduto = document.getElementById("produto");
   const produtoSelecionado = selectProduto.value;
-  const partes = produtoSelecionado.split(" - ");
-  const nomeProduto = partes[0]; // Nome do produto
-  const valorUnitario = parseFloat(
-    partes[1].replace("R$ ", "").replace(",", ".")
-  ); // Remove 'R$ ' e substitui ',' por '.'
 
+  const partes = produtoSelecionado.split(" - ");
+  const nomeProduto = partes[0]; // Nome do produto, primeira parte do array
+  const valorUnitario = parseFloat(
+    partes[1].replace("R$", "").replace(",", ".")
+  ); // Remove 'R$ ' e substitui ',' por '.'
   const selectQuantidade = document.getElementById("quantidade");
   const quantidade = parseInt(selectQuantidade.value);
-
   const subtotal = valorUnitario * quantidade;
+
   const listaProdutos = document.getElementById("lista-produtos");
   const itemCarrinho = document.createElement("section");
   itemCarrinho.classList.add("carrinho__produtos__produto");
   itemCarrinho.innerText = `${quantidade}x ${nomeProduto} - R$ ${valorUnitario.toFixed(
     2
   )}`;
-
   listaProdutos.appendChild(itemCarrinho);
+
+  total += subtotal;
+  const totalElement = document.getElementById("valor-total");
+  totalElement.innerText = `R$ ${total.toFixed(2)}`;
+
+  
+}
+function limpar() {
+  document.getElementById("lista-produtos").innerHTML = ""; // Limpa a lista de produtos
+  total = 0; // Reseta o total
+  const totalElement = document.getElementById("valor-total").innerText = `R$ ${total.toFixed(2)}`; // Mostra o valor total resetado na tela
 }
