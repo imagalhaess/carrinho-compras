09b05890d7590be9ac89303cea12f3b640d466cd
Isabela M. <magalhaes569@gmail.com>
2025-05-16 16:57:43 -0300

    refactor: removendo innerHTML e utilizando createElement e appendChild para melhoria

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,23 @@ limpar ();
 function adicionar() {
   // Recupera o valores nome do produto, valor e a quantidade
   let produto = document.getElementById("produto").value;
-  let nomeProduto = produto.split(" - ")[0];
-  let valorUnitario = produto.split("R$")[1];
+  let [nomeProduto, valorStr] = produto.split(" - ");
+  let valorUnitario = parseFloat(valorStr.replace("R$", "").replace(",", "."));
   let quantidade = document.getElementById("quantidade").value;
+
   // Calcular o subtotal
   let subtotal = valorUnitario * quantidade;
   let carrinho = document.getElementById("lista-produtos");
+
   // Adiciona o produto ao carrinho
-  carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}</span>x ${nomeProduto}  <span class="texto-azul"> R$${subtotal}</span>
-        </section>`
+  let novoItem = document.createElement("section");
+novoItem.classList.add("carrinho__produtos__produto");
+novoItem.innerHTML = `
+  <span class="texto-azul">${quantidade}</span>x ${nomeProduto}
+  <span class="texto-azul"> R$${subtotal}</span>
+`;
+carrinho.appendChild(novoItem);
+
   // Atualiza o valor total
   total += subtotal;
   // Atualiza o valor total na tela
